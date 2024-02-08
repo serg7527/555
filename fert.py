5a75bb2dc1cee6a20f4bc07678ed9d73d13bc2bf
Searhei <156844838+serg7527@users.noreply.github.com>
2024-02-08 21:23:57 +0300

    psutil2

---
+++ fert.py
@@ -0,0 +1,7 @@
+def sum_nums(a, b, c):
+    sum = a + (b - c)
+    return sum
+print("is not")
+
+first_sum = sum_nums(20, 16, 15)
+print(first_sum)
--- pisc.py
+++ pisc.py
@@ -1,1 +1,7 @@
-print("Hell0 world")
+N = 4
+matrix = list()
+for i in range(N):
+    matrix.append(list())
+    for j in range(N):
+        matrix[i].append(0)
+        print(matrix[i])

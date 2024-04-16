a3acac7bb4106a29b432f532e95ebeecb837d83e
Searhei <156844838+serg7527@users.noreply.github.com>
2024-04-17 01:33:46 +0300

    17/04/24 Django

--- app1/main/views.py
+++ app1/main/views.py
@@ -6,13 +6,20 @@ from django.template import context
 
 def index(request):
     context: dict = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
-
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Самый нужный текст на этом сайте'
+    }
+
+    return render(request, 'main/about.html', context)
+
+

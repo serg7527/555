d6f7a4552caa6c09b315c9cda89285ae27c3e6e9
Searhei <156844838+serg7527@users.noreply.github.com>
2024-04-19 00:38:56 +0300

    19/04/24 django

--- app1/main/views.py
+++ app1/main/views.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import context
 
+from goods.models import Categories
+
 
 
 def index(request):
+
+    categories = Categories.objects.all()
     context: dict = {
         'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME'
+        'content': 'Магазин мебели HOME',
+        'categories': categories
     }
 
     return render(request, 'main/index.html', context)

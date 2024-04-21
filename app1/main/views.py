f412e3c04ddf098db85da6178ce5c818ecae0ead
Searhei <156844838+serg7527@users.noreply.github.com>
2024-04-22 01:35:21 +0300

    22/04/24___INIT__

--- app1/main/views.py
+++ app1/main/views.py
@@ -1,28 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-#from django.template import context
+
 from goods.models import Categories
 
 
 def index(request):
 
-    categories = Categories.objects.all()
-    context: dict = {
-        'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME',
-        'categories': categories
+    context = {
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    context: dict = {
-        'title': 'Home - О нас',
-        'content': 'О нас',
-        'text_on_page': 'Самый нужный текст на этом сайте'
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Самый нужный текст на этом сайте",
     }
 
     return render(request, 'main/about.html', context)
-
-

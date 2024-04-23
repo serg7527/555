cf06d1fcc0945d200966569acd14d971ef5307a8
Searhei <156844838+serg7527@users.noreply.github.com>
2024-04-24 00:18:35 +0300

    24/04/24_django

--- app1/goods/views.py
+++ app1/goods/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from goods.models import Products
 
 
@@ -15,5 +14,13 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product':product
+    }
+
+
+    return render(request, "goods/product.html",context=context)
